f5614aa0f0f94e04bdfa4ab70e49bbf8a1154af3
JohnGuerreroCor <john.guerrero@usco.edu.co>
2023-07-12 08:04:57 -0500

    ajustes angular.json y variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,7 @@
 export const environment = {
   production: false,
   //DESCOMENTAR LOCAL
-  URL_BACKEND : "http://localhost:8080/encuestas-administrador-1.0",
+  URL_BACKEND: 'http://localhost:8080/encuestas-administrador-1.0',
   //DESCOMENTAR PRODUCCIÓN
-  //URL_BACKEND : "https://sanagustin.usco.edu.co/carnetizacion_admon",
-  firebase: {
-    apiKey: 'AIzaSyDCI1DjDRTyWgCJd681nhrEqY216Kfbm44',
-    authDomain: 'doctoradocienciasdelasaludusco.firebaseapp.com',
-    databaseURL: 'https://doctoradocienciasdelasaludusco.firebaseio.com',
-    projectId: 'doctoradocienciasdelasaludusco',
-    storageBucket: 'doctoradocienciasdelasaludusco.appspot.com',
-    messagingSenderId: '399106387464',
-    appId: '1:399106387464:web:7741df6f88e489f7176bbd',
-    measurementId: 'G-PTHMQCDL1F'
-  }
+  //URL_BACKEND : "https://sanagustin.usco.edu.co/encuestas_admon",
 };
